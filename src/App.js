a814914f53b21e804f0fdf2f4075f78671f08310
merrygold <66420624+merrygold@users.noreply.github.com>
2020-06-04 20:21:07 +0500

    Update1

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+         <code>Bootcamp 2020</code>
+        </p>
+        <a
+          className="App-link"
+          href="#"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Project By Umair
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
