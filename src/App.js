360cdb273a687fe3627db177cd974dba0c5321e0
merrygold <66420624+merrygold@users.noreply.github.com>
2020-06-13 02:47:09 +0500

    Project Name Updated

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Project By Umair
+          Project By Muhammad Umair
         </a>
       </header>
     </div>
